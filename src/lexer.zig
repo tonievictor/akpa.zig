0d07a4aea46c44849b6b84910b5e419c64b9c989
Tonie Victor <victornwanochi@gmail.com>
2025-06-18 23:09:43 +0100

    implemented the lexer for a very small subset of the sql spec

---
+++ src/lexer.zig
@@ -0,0 +1,170 @@
+const std = @import("std");
+
+const KeywordVariant = enum {
+    Create,
+    Table,
+    Primary,
+    Key,
+    Int,
+};
+
+const TokenType = union(enum) {
+    keyword: KeywordVariant,
+    identifier: []const u8,
+    number_literal: i32,
+    equal: u8,
+    semicolon: u8,
+    oparen: u8,
+    cparen: u8,
+};
+
+const Token = struct {
+    token_type: TokenType,
+    line: i32,
+    col: i32,
+
+    pub fn init(ttype: TokenType, line: i32, col: i32) Token {
+        return Token{
+            .token_type = ttype,
+            .line = line,
+            .col = col,
+        };
+    }
+};
+
+const LexerError = error{
+    IllegalCharacter,
+};
+
+pub fn free_tokens(allocator: std.mem.Allocator, tokens: []Token) void {
+    for (tokens) |token| {
+        switch (token.token_type) {
+            .identifier => |i| {
+                allocator.free(i);
+            },
+            else => {
+                continue;
+            },
+        }
+    }
+    allocator.free(tokens);
+}
+
+pub fn tokenize(allocator: std.mem.Allocator, code: []const u8) ![]Token {
+    var index: u32 = 0;
+    var line: i32 = 1;
+    var col: i32 = 1;
+    var tokens = std.ArrayList(Token).init(allocator);
+
+    while (index < code.len) {
+        var token: Token = undefined;
+        const char = code[index];
+        switch (char) {
+            '\n' => {
+                line += 1;
+                col = 1;
+                index += 1;
+                continue;
+            },
+            ' ', '\t' => {
+                col += 1;
+                index += 1;
+                continue;
+            },
+            ';' => {
+                const ttype = TokenType{ .semicolon = ';' };
+                token = Token.init(ttype, line, col);
+                col += 1;
+                index += 1;
+            },
+            '(' => {
+                const ttype = TokenType{ .oparen = '(' };
+                token = Token.init(ttype, line, col);
+                col += 1;
+                index += 1;
+            },
+            ')' => {
+                const ttype = TokenType{ .cparen = ')' };
+                token = Token.init(ttype, line, col);
+                col += 1;
+                index += 1;
+            },
+            '=' => {
+                const ttype = TokenType{ .equal = '=' };
+                token = Token.init(ttype, line, col);
+                col += 1;
+                index += 1;
+            },
+            '0'...'9' => {
+                const amv = try extract_number_literal(code, index, col, line);
+                token = amv.t;
+                index = amv.i;
+                col = amv.c;
+            },
+            'A'...'Z', 'a'...'z' => {
+                const amv = try extract_character_sequence(allocator, code, index, col, line);
+                token = amv.t;
+                index = amv.i;
+                col = amv.c;
+            },
+            else => {
+                return LexerError.IllegalCharacter;
+            },
+        }
+        try tokens.append(token);
+    }
+    return try tokens.toOwnedSlice();
+}
+
+fn extract_number_literal(code: []const u8, start_index: u32, start_col: i32, line: i32) !struct { t: Token, i: u32, c: i32 } {
+    var index = start_index;
+    var col = start_col;
+    while (index < code.len) {
+        switch (code[index]) {
+            '0'...'9' => {
+                index += 1;
+                col += 1;
+            },
+            else => break,
+        }
+    }
+    const value = try std.fmt.parseInt(i32, code[start_index..index], 0);
+    const ttype = TokenType{ .number_literal = value };
+    const token = Token.init(ttype, line, start_col);
+    return .{ .t = token, .i = index, .c = col };
+}
+
+fn extract_character_sequence(allocator: std.mem.Allocator, code: []const u8, start_index: u32, start_col: i32, line: i32) !struct { t: Token, i: u32, c: i32 } {
+    var index = start_index;
+    var col = start_col;
+    while (index < code.len) {
+        switch (code[index]) {
+            'A'...'Z', 'a'...'z' => {
+                index += 1;
+                col += 1;
+            },
+            else => break,
+        }
+    }
+
+    const ttype = try make_identifier_or_keyword(allocator, code[start_index..index]);
+    const token = Token.init(ttype, line, start_col);
+    return .{ .t = token, .i = index, .c = col };
+}
+
+fn make_identifier_or_keyword(allocator: std.mem.Allocator, string: []const u8) !TokenType {
+    if ((std.mem.eql(u8, string, "CREATE")) or (std.mem.eql(u8, string, "create"))) {
+        return TokenType{ .keyword = KeywordVariant.Create };
+    } else if ((std.mem.eql(u8, string, "TABLE")) or (std.mem.eql(u8, string, "table"))) {
+        return TokenType{ .keyword = KeywordVariant.Table };
+    } else if ((std.mem.eql(u8, string, "PRIMARY")) or (std.mem.eql(u8, string, "primary"))) {
+        return TokenType{ .keyword = KeywordVariant.Primary };
+    } else if ((std.mem.eql(u8, string, "KEY")) or (std.mem.eql(u8, string, "key"))) {
+        return TokenType{ .keyword = KeywordVariant.Key };
+    } else if ((std.mem.eql(u8, string, "INT")) or (std.mem.eql(u8, string, "int"))) {
+        return TokenType{ .keyword = KeywordVariant.Int };
+    } else {
+        const copied = try allocator.dupe(u8, string);
+        return TokenType{ .identifier = copied };
+    }
+}
--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,32 @@
 const std = @import("std");
+const lexer = @import("lexer.zig");
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const leak_check = gpa.deinit();
+        if (leak_check == .leak) {
+            std.debug.print("Memory leak detected!\n", .{});
+        }
+    }
+    const allocator = gpa.allocator();
+
     while (true) {
         try print_dollar();
-        const line = try getInput();
-        defer std.heap.page_allocator.free(line);
-
+        const line = try getInput(allocator);
+        defer allocator.free(line);
         if (std.mem.eql(u8, line, "quit") == true) {
             return;
         }
+
+        const tokens = try lexer.tokenize(allocator, line);
+        defer (lexer.free_tokens(allocator, tokens));
     }
 }
 
-fn getInput() ![]const u8 {
+fn getInput(allocator: std.mem.Allocator) ![]const u8 {
     const stdin = std.io.getStdIn().reader();
-    const line = try stdin.readUntilDelimiterAlloc(std.heap.page_allocator, '\n', 8192);
+    const line = try stdin.readUntilDelimiterAlloc(allocator, '\n', 8192);
 
     return std.mem.trim(u8, line, "\r");
 }
@@ -23,4 +35,3 @@ fn print_dollar() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("$> ", .{});
 }
-
